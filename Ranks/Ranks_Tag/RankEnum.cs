f301dcda91c7107cb080c05e0dc930c6b6edd96f
Vitor Darela <vitordarela@hotmail.com>
2024-08-01 17:07:17 -0300

    add tags

---
+++ Ranks/Ranks_Tag/RankEnum.cs
@@ -0,0 +1,54 @@
+﻿public enum Rank
+{
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Eleven = 11,
+    Twelve = 12,
+    Thirteen = 13,
+    Fourteen = 14,
+    Fifteen = 15
+}
+
+public static class RankHelper
+{
+    public static string GetRankSymbol(Rank rank)
+    {
+        // Mapeia os valores do enum para os símbolos Unicode
+        switch (rank)
+        {
+            case Rank.One: return "①";
+            case Rank.Two: return "②";
+            case Rank.Three: return "③";
+            case Rank.Four: return "④";
+            case Rank.Five: return "⑤";
+            case Rank.Six: return "⑥";
+            case Rank.Seven: return "⑦";
+            case Rank.Eight: return "⑧";
+            case Rank.Nine: return "⑨";
+            case Rank.Ten: return "⑩";
+            case Rank.Eleven: return "⑪";
+            case Rank.Twelve: return "⑫";
+            case Rank.Thirteen: return "⑬";
+            case Rank.Fourteen: return "⑭";
+            case Rank.Fifteen: return "⑮";
+            default: return "";
+        }
+    }
+
+    public static Rank? GetRankFromKey(string key)
+    {
+        if (Enum.TryParse(typeof(Rank), key, out var rank))
+        {
+            return (Rank)rank;
+        }
+        return null;
+    }
+}
--- Ranks/Ranks_Tag/Ranks_Tag.cs
+++ Ranks/Ranks_Tag/Ranks_Tag.cs
@@ -1,11 +1,14 @@
-﻿using System.Text.RegularExpressions;
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Timers;
-using Dapper;
-using MySqlConnector;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Logging;
 using RanksApi;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+using System.Text.Json.Serialization;
 
 namespace Ranks_Tag;
 
@@ -19,18 +22,74 @@ public class RanksTag : BasePlugin
 
     public override void OnAllPluginsLoaded(bool hotReload)
     {
+        Logger.LogInformation("Loading Rank T....");
+
         _api = IRanksApi.Capability.Get();
         if (_api == null) return;
+    }
 
+
+    [GameEventHandler]
+    public HookResult OnClientConnect(EventPlayerConnectFull @event, GameEventInfo info)
+    {
+
+        CCSPlayerController player = @event.Userid;
+        if (player == null || !player.IsValid || player.IsBot)
+        {
+            return HookResult.Continue;
+
+        }
+
+        //ADD 5 seconts to wait the VIP set
         AddTimer(5f, () =>
         {
-            foreach (var player in Utilities.GetPlayers().Where(u => u.IsValid))
+            Logger.LogInformation("Player {Name} has connected!", player.PlayerName);
+            var level = _api.GetLevelFromExperience(_api.GetPlayerExperience(player));
+
+            Logger.LogInformation($"The current user rank is '{level.Name}'");
+
+            string rankSymbol = "";
+            if (Enum.TryParse<Rank>(level.Name, out Rank rank))
             {
-                var level = _api.GetLevelFromExperience(_api.GetPlayerExperience(player));
-                
-                player.Clan = $"[{Regex.Replace(level.Name, @"\{[A-Za-z]+}", "")}]";
-                Utilities.SetStateChanged(player, "CCSPlayerController", "m_szClan");
+                rankSymbol = RankHelper.GetRankSymbol(rank);
+                Logger.LogInformation($"The rank symbol for key '{level.Name}' is: {rankSymbol}");
             }
-        }, TimerFlags.REPEAT);
+            else
+            {
+                Logger.LogInformation($"Rank not found for key '{level.Name}'");
+            }
+
+            player.Clan = $"{player.Clan} {rankSymbol}";
+            Utilities.SetStateChanged(player, "CCSPlayerController", "m_szClan");
+
+        });
+
+        
+       
+        AddTimer(10f, () =>
+        {
+            player.PrintToChat($"{ChatColors.Gold}[ClutchArena] {ChatColors.DarkRed}www.ClutchArena.com.br");
+            player.PrintToChat($"{ChatColors.Gold}[ClutchArena] {ChatColors.LightBlue}Olá {player.PlayerName} acesse nosso site e concorra a prêmios em SKIN todo mês");
+            player.PrintToChat($"{ChatColors.Gold}[ClutchArena] {ChatColors.Green}Torne-se um membro VIP {ChatColors.LightYellow}★ {ChatColors.Green} e aproveite várias vantagens, além de participar de sorteios exclusivos!");
+        });
+
+
+        AddTimer(35f, () =>
+        {
+            player.PrintToCenter($"{player.PlayerName} junte-se ao nosso Discord! Faça parte desta comunidade incrível");
+        });
+
+
+        return HookResult.Continue;
     }
+}
+
+public class Advertisement
+{
+    public float Interval { get; init; }
+    public List<Dictionary<string, string>> Messages { get; init; } = null!;
+
+    private int _currentMessageIndex;
+
+    [JsonIgnore] public Dictionary<string, string> NextMessages => Messages[_currentMessageIndex++ % Messages.Count];
 }
